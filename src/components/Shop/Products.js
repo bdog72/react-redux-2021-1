333e1cac2e39b3c6974a60202a014c71fbdd6f4f
Brian Bycynski <high5bri@hotmail.com>
2021-11-21 03:18:05 -0500

    redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,26 +4,26 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-const DUMMY_PRODUCTS = [
-  {
-    id: 'p1',
-    price: 6,
-    title: 'My First Book',
-    description: 'The first book I ever wrote',
-  },
-  {
-    id: 'p2',
-    price: 3,
-    title: 'My Second Book',
-    description: 'The second book I ever wrote',
-  },
-  {
-    id: 'p3',
-    price: 10,
-    title: 'My Third Book',
-    description: 'The third book I ever wrote',
-  },
-];
+// const DUMMY_PRODUCTS = [
+//   {
+//     id: 'p1',
+//     price: 6,
+//     title: 'My First Book',
+//     description: 'The first book I ever wrote',
+//   },
+//   {
+//     id: 'p2',
+//     price: 3,
+//     title: 'My Second Book',
+//     description: 'The second book I ever wrote',
+//   },
+//   {
+//     id: 'p3',
+//     price: 10,
+//     title: 'My Third Book',
+//     description: 'The third book I ever wrote',
+//   },
+// ];
 
 const Products = (props) => {
   return (
