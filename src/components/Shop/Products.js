96375754066c0fe61236becd8d34411eb1d626a2
Brian Bycynski <high5bri@hotmail.com>
2021-11-22 02:51:33 -0500

    redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -4,37 +4,48 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-// const DUMMY_PRODUCTS = [
-//   {
-//     id: 'p1',
-//     price: 6,
-//     title: 'My First Book',
-//     description: 'The first book I ever wrote',
-//   },
-//   {
-//     id: 'p2',
-//     price: 3,
-//     title: 'My Second Book',
-//     description: 'The second book I ever wrote',
-//   },
-//   {
-//     id: 'p3',
-//     price: 10,
-//     title: 'My Third Book',
-//     description: 'The third book I ever wrote',
-//   },
-// ];
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 10,
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
+  },
+  {
+    id: 'p2',
+    price: 8,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
+  },
+  {
+    id: 'p3',
+    price: 6,
+    title: 'My Third Book',
+    description: 'The third book I ever wrote',
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
+        {/* <ProductItem
           title='Test'
           price={6}
           description='This is a first product - amazing!'
-        />
+        /> */}
       </ul>
     </section>
   );
